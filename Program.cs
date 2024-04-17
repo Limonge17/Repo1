19b3d7e57929b540c2717b076eb3f71c5b162578
Thiago Limonge Santana <156456325+Limonge17@users.noreply.github.com>
2024-04-17 01:43:47 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n, i;
+        Console.WriteLine("Informe uma quantidade: ");
+        n = int.Parse(Console.ReadLine());
+        for (i = 1; i <= n; i++)
+        {
+            Console.WriteLine($"Os primeiros {n} números pares são: {i * 2}");        
+        }
+        for (i = 1; i <= n; i++)
+        {
+            Console.WriteLine($"Os primeiros {n} números impares são: {i * 2 - 1}");        
+        }
+
+}
+}
